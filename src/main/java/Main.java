b7a8694dd36b8922a95af8fcccca051f82cbef93
Aleksandr Kovsharov <sanforg2@gmail.com>
2019-05-26 10:08:28 +0300

    Added utils

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,17 @@
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Rectangle;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.opencv.core.*;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
+import utils.Utils;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -40,11 +23,12 @@ public class Main extends Application {
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
     private VideoCapture capture = new VideoCapture();
-    private int cameraId = 0;
+    private static final int cameraId = 0;
+    private static final int color = Imgproc.COLOR_BGR2GRAY;
+
     private ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
     private CascadeClassifier eye = new CascadeClassifier(Paths.get("haarcascade_eye.xml").toString());
     private CascadeClassifier face = new CascadeClassifier(Paths.get("haarcascade_frontalface_default.xml").toString());
-    private int cvt = Imgproc.COLOR_BGR2GRAY;
     private ImageView view = new ImageView();
     public static void main(String[] args) {
         launch(args);
@@ -68,9 +52,9 @@ public class Main extends Application {
                 Mat frame = grabFrame();
                 MatOfRect faceRects = detect(frame, face);
                 MatOfRect eyesRects = detectEyes(frame, faceRects);
-                drawRects(eyesRects.toArray(), frame);
-                drawRects(faceRects.toArray(), frame);
-                Image imageToShow = mat2Image(frame);
+                Utils.drawRects(eyesRects.toArray(), frame);
+                Utils.drawRects(faceRects.toArray(), frame);
+                Image imageToShow = Utils.mat2Image(frame);
                 Platform.runLater(() -> view.imageProperty().set(imageToShow));
             };
 
@@ -82,10 +66,6 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    private boolean isInRect(Rect outer, Rect inner){
-        return outer.contains(new Point(inner.x, inner.y)) && inner.x + outer.x + inner.width < outer.width
-                && inner.y + outer.y + inner.height < outer.height;
-    }
 
     private MatOfRect detectEyes(Mat frame, MatOfRect rect){
         MatOfRect eyes = new MatOfRect();
@@ -107,12 +87,6 @@ public class Main extends Application {
         return eyes;
     }
 
-    private void drawRects(Rect[] rects, Mat frame){
-        for(Rect rect : rects)
-            Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
-                    new Scalar(50, 255, 50),2);
-    }
-
     private Mat grabFrame()
     {
         // init everything
@@ -130,7 +104,7 @@ public class Main extends Application {
                 // if the frame is not empty, process it
                 if (!frame.empty())
                 {
-                    Imgproc.cvtColor(frame, frame, cvt);
+                    Imgproc.cvtColor(frame, frame, color);
                 }
                 else {
                     System.out.println("Frame empty");
@@ -147,39 +121,6 @@ public class Main extends Application {
         return frame;
     }
 
-    public static Image mat2Image(Mat frame)
-    {
-        try
-        {
-            return SwingFXUtils.toFXImage(matToBufferedImage(frame), null);
-        }
-        catch (Exception e)
-        {
-            System.err.println("Cannot convert the Mat obejct: " + e);
-            return null;
-        }
-    }
-
-    private static BufferedImage matToBufferedImage(Mat original)
-    {
-        BufferedImage image = null;
-        int width = original.width(), height = original.height(), channels = original.channels();
-        byte[] sourcePixels = new byte[width * height * channels];
-        original.get(0, 0, sourcePixels);
-
-        if (original.channels() > 1)
-        {
-            image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-        }
-        else
-        {
-            image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-        }
-        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-        System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
-
-        return image;
-    }
 
     @Override
     public void stop() throws Exception {
---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,66 @@
+package utils;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+public class Utils {
+
+    private Utils(){
+
+    }
+
+    private static BufferedImage matToBufferedImage(Mat original)
+    {
+        BufferedImage image = null;
+        int width = original.width(), height = original.height(), channels = original.channels();
+        byte[] sourcePixels = new byte[width * height * channels];
+        original.get(0, 0, sourcePixels);
+
+        if (original.channels() > 1)
+        {
+            image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        }
+        else
+        {
+            image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        }
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
+
+        return image;
+    }
+
+    public static Image mat2Image(Mat frame)
+    {
+        try
+        {
+            return SwingFXUtils.toFXImage(matToBufferedImage(frame), null);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Cannot convert the Mat obejct: " + e);
+            return null;
+        }
+    }
+
+    public static void drawRects(Rect[] rects, Mat frame){
+        for(Rect rect : rects)
+            Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(50, 255, 50),2);
+    }
+
+    public static boolean isInRect(Rect outer, Rect inner){
+        return outer.contains(new Point(inner.x, inner.y)) && inner.x + outer.x + inner.width < outer.width
+                && inner.y + outer.y + inner.height < outer.height;
+    }
+
+
+}
