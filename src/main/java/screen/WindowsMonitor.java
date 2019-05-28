7b4baaeca5883a27ae0422da0d4f46ae172aa1b4
Aleksandr Kovsharov <sanforg2@gmail.com>
2019-05-28 07:41:45 +0300

    Changed WindowsMonitor

--- src/main/java/screen/WindowsMonitor.java
+++ src/main/java/screen/WindowsMonitor.java
@@ -10,23 +10,19 @@ public enum WindowsMonitor implements Screen
     private boolean isOn = true;
 
     public void turnOn(){
-        if(!isOn()){
-            user.SendMessageA(WinUser.HWND_BROADCAST, WinUser.WM_SYSCOMMAND,
-                    User32.SC_MONITORPOWER, new WinDef.LPARAM(User32.SC_MONITOR_ON));
-            isOn = true;
-        }
+        user.SendMessageA(WinUser.HWND_BROADCAST, WinUser.WM_SYSCOMMAND,
+                User32.SC_MONITORPOWER, new WinDef.LPARAM(User32.SC_MONITOR_ON));
+        isOn = true;
     }
     public void turnOff(){
-        if(isOn()){
-            user.SendMessageA(WinUser.HWND_BROADCAST, WinUser.WM_SYSCOMMAND,
-                    User32.SC_MONITORPOWER, new WinDef.LPARAM(User32.SC_MONITOR_OFF));
-            isOn = false;
-        }
+        user.SendMessageA(WinUser.HWND_BROADCAST, WinUser.WM_SYSCOMMAND,
+                User32.SC_MONITORPOWER, new WinDef.LPARAM(User32.SC_MONITOR_OFF));
+        isOn = false;
     }
 
     @Override
     public void setBrightness(double value) {
-
+        
     }
 
 
